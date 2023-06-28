3ec2562f3273af62ba0de87c58f2c3022457b87c
adrianrojas2019 <rojasadrian@hotmail.com>
2023-06-28 13:17:40 -0600

    step definitions for google test were added

--- SpecFlowProject1/StepDefinitions/SearchInGoogleHomePageStepDefinitions.cs
+++ SpecFlowProject1/StepDefinitions/SearchInGoogleHomePageStepDefinitions.cs
@@ -1,65 +1,60 @@
+using AventStack.ExtentReports.Gherkin.Model;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.BindingSkeletons;
 
 namespace SpecFlowProjectUsingBDD.StepDefinitions
 {
     [Binding]
     public class SearchInGoogleHomePageStepDefinitions
     {
+        IWebDriver? driver;
         [Given(@"Google Page Open")]
         public void GivenGooglePageOpen()
         {
-
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://www.google.com");
         }
 
         [Given(@"Search Text Box should be present and Enabled in the Google Home Page")]
         public void GivenSearchTextBoxShouldBePresentAndEnabledInTheGoogleHomePage()
         {
-
+            Console.WriteLine(driver.Title);
+            Assert.That(driver.FindElement(By.Name("q")).Displayed, Is.True);
+            Assert.That(driver.FindElement(By.Name("q")).Enabled, Is.True);
         }
 
-        [When(@"User searches a Course with a keyword Java Tutorial")]
-        public void WhenUserSearchesACourseWithAKeywordJavaTutorial()
+        [When(@"User searches a Course with a keyword (.*) Tutorial")]
+        public void WhenUserSearchesACourseWithAKeywordJavaTutorial(string keyWord)
         {
-
+            // create into new page object
+            //public IWebElement SearchBox => driver.FindElement(By.Name("q"));
+            var searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys(keyWord);
+            Thread.Sleep(100);
+            searchBox.SendKeys(Keys.Escape);
+            Thread.Sleep(100);
         }
 
         [When(@"Hit enters button")]
         public void WhenHitEntersButton()
         {
-
+            var searchBox = driver.FindElement(By.Name("q"));
+            //searchBox.Click();
+            //another workaround
+            searchBox.SendKeys(Keys.Enter);
         }
 
-        [Then(@"All Courses related to Java Tutorial should be displayed")]
-        public void ThenAllCoursesRelatedToJavaTutorialShouldBeDisplayed()
+        [Then(@"All Courses related to (.*) Tutorial should be displayed")]
+        public void ThenAllCoursesRelatedToJavaTutorialShouldBeDisplayed(string keyWord)
         {
-
+            //Validate keyword has been displayed
+            Assert.IsTrue(driver.Url.Contains(keyWord));
+            driver.Quit();
         }
-
-        [When(@"User searches a Course with a keyword SpecFlow Tutorial")]
-        public void WhenUserSearchesACourseWithAKeywordSpecFlowTutorial()
-        {
-
-        }
-
-        [Then(@"All Courses related to SpecFlow Tutorial should be displayed")]
-        public void ThenAllCoursesRelatedToSpecFlowTutorialShouldBeDisplayed()
-        {
-
-        }
-
-        [When(@"User searches a Course with a keyword Selenium Tutorial")]
-        public void WhenUserSearchesACourseWithAKeywordSeleniumTutorial()
-        {
-
-        }
-
-        [Then(@"All Courses related to Selenium Tutorial should be displayed")]
-        public void ThenAllCoursesRelatedToSeleniumTutorialShouldBeDisplayed()
-        {
-
-        }
-
-
     }
 }
